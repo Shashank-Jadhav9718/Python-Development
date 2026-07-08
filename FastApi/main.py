fbe3262b8c0d2645a1a711f75d22a2192471dcd2
Shashank-Jadhav9718 <21sbjadhav19092@gmail.com>
2026-07-08 19:15:51 +0530

    Day 5

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI , HTTPException , Depends , Header
 from pydantic import BaseModel , Field
 
 app = FastAPI()
+
+def verify_api_key(X_api_key : str = Header(None)):
+    if X_api_key != "super_secret_api_key":
+        raise HTTPException(status_code = 401 , detail = "Unauthorized : Invalid API key")
+    
+    return X_api_key
 
 class CreateUser(BaseModel):
     Username : str = Field(..., min_length = 3, max_length = 8)
     email : str 
     password : str = Field(..., min_length = 8, max_length = 16 )
+    
     
 class User_response(BaseModel):
     Username : str 
@@ -16,3 +23,15 @@ class User_response(BaseModel):
 def create_user(new_user : CreateUser):
     return new_user
 
+@app.get("/public")
+def public_info():
+    return {"Message For Everyone"}
+
+@app.get("/secure_data")
+def secure_data(api_key : str = Depends(verify_api_key)):
+    return {
+        "Message" : "Welcome To VIP access",
+        "Key used" : api_key
+    }
+    
+    
