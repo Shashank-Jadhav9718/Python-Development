bf37ba1568f0511ff8c0537cc73646c4221b8039
Shashank-Jadhav9718 <21sbjadhav19092@gmail.com>
2026-07-16 00:24:23 +0530

    Day 7

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,10 +1,48 @@
-from fastapi import FastAPI
-import Users
+from fastapi import FastAPI , Depends , HTTPException
+from pydantic import BaseModel
+from sqlalchemy import Column , Integer , String
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal, Base
 
+class DBUser(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String)
+    
+Base.metadata.create_all(bind=engine)
+
+class UserCrate(BaseModel):
+    username : str
+    email : str
+    
 app = FastAPI()
 
-app.include_router(Users.router)
-
-@app.get("/")
-def front_door():
-    return {"message": "Welcome to the Main Mall!"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@app.post('/Users')
+def create_user(user : UserCrate , db : Session = Depends(get_db)):
+    new_user = DBUser(username=user.username , email=user.email)
+    db.add(new_user)
+    try:
+        db.commit()
+        db.refresh(new_user)
+    except Exception as e :
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Username Already Exists")
+    
+    
+    return {
+        "message": "User saved securely to PostgreSQL!", 
+        "user": {
+            "id": new_user.id,
+            "username": new_user.username,
+            "email": new_user.email
+        }
+    }
