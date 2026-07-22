e557eec87c7f751bea0101e1f6eab15b591f662c
Shashank-Jadhav9718 <21sbjadhav19092@gmail.com>
2026-07-22 17:11:22 +0530

    Day 14

--- FastApi/models.py
+++ FastApi/models.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 class DBUser(Base):
-    __tablename__ = "users"
-
+    __tablename__ = 'users'
+    
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False, server_default='temp_password')
 
     posts = relationship("DBPost", back_populates="owner", cascade="all, delete")
 
