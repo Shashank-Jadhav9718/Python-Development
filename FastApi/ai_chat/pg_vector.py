30d34f8933a20f4cabc20713b1a929cf7bb9333e
Shashank-Jadhav9718 <21sbjadhav19092@gmail.com>
2026-08-24 16:59:13 +0530

    Day 8 AI

---
+++ FastApi/ai_chat/pg_vector.py
@@ -0,0 +1,80 @@
+import os
+from google import genai
+from sqlalchemy import create_engine, Text, Column, Integer, select
+from sqlalchemy.orm import declarative_base, sessionmaker
+from pgvector.sqlalchemy import Vector 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+DB_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+Base = declarative_base()
+
+class Document(Base):
+    __tablename__ = "documents"
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    embedding = Column(Vector(3072), nullable=False)
+    
+def pg_vector_pipeline():
+    api_key = os.getenv('GEMINI_SECRET_KEY')
+    if not api_key:
+        raise ValueError("GEMINI_SECRET_KEY is missing from .env.")
+    
+    client = genai.Client(api_key=api_key)
+    print("🚀 Booting up the Embedding Engine...\n")
+
+    print("🛠️ Syncing tables to PostgreSQL...")
+    Base.metadata.create_all(bind=engine)
+    
+    db = SessionLocal()
+    try:
+        db.query(Document).delete()
+        db.commit()
+        
+        documents = [
+            "FastAPI is a Python framework used to create backend web servers and APIs.",
+            "Dogs are highly social animals and love to play.",
+            "To build AI apps, you need API keys and cloud deployment."
+        ]
+
+        print("🔍 Generating embeddings and inserting into pgvector...")
+        for doc in documents:
+            result = client.models.embed_content(
+                model='gemini-embedding-2',
+                contents=doc
+            )    
+            embeddings = result.embeddings[0].values
+            
+            db_doc = Document(content=doc , embedding=embeddings)
+            db.add(db_doc)
+            
+        db.commit()
+        print("✅ Data successfully saved!\n")
+        
+        user_question = "How do I create a backend web server?"
+        print(f"👤 Search Query: '{user_question}'")
+        
+        query_result=client.models.embed_content(
+            model='gemini-embedding-2',
+            contents=user_question
+        )
+        query_vector = query_result.embeddings[0].values
+        
+        stmt = select(Document).order_by(
+            Document.embedding.cosine_distance(query_vector)
+        ).limit(1)
+        
+        top_match = db.execute(stmt).scalars().first()
+        print(f"\n🏆 Top Database Match: '{top_match.content}'")
+        
+    finally:
+        db.close()
+        
+        
+if __name__ == "__main__":
+    pg_vector_pipeline()
